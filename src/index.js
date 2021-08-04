a26802d9cc375fd659577f6c03b1bee2b9cc5e07
Natalia Pavlova <pavlova_li@ukr.net>
2021-08-05 02:28:51 +0300

    food-service

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import items from './menu.json';
+import itemTemplate from './templates/menu-item.hbs';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const itemsList = document.querySelector('.js-menu');
+const menuItems = items.map(itemTemplate).join('');
+itemsList.innerHTML = menuItems;
+const theme = localStorage.getItem('theme');
+const themeToggle = document.querySelector('#theme-switch-toggle');
+
+if (theme === 'DARK') {
+  themeToggle.checked = true;
+  document.body.classList.add(Theme.DARK);
+} else {
+  document.body.classList.add(Theme.LIGHT);
+}
+
+themeToggle.addEventListener('change', () => {
+  const bodyElem = document.body;
+  const newTheme = bodyElem.classList.contains(Theme.LIGHT) ? 'DARK' : 'LIGHT';
+  const oldTheme = !bodyElem.classList.contains(Theme.LIGHT) ? 'DARK' : 'LIGHT';
+  bodyElem.classList.remove(Theme[oldTheme]);
+  bodyElem.classList.add(Theme[newTheme]);
+  localStorage.setItem('theme', newTheme);
+});
